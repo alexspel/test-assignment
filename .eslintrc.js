496f7976a4fb709d8a71f5c33e5716402ada43bc
alexspel <staflin18@gmail.com>
2024-02-01 21:47:21 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:eslint-plugin-prettier/recommended',
+        'prettier',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'require-jsdoc': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-types': 'off',
+    },
+};
